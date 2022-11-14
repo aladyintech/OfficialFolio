dfa3b3f92feb49e2146acf5ec0544b440934728d
Samia <68406432+samia-gits-it@users.noreply.github.com>
2022-11-14 15:34:33 -0600

    some msg

---
+++ db/db.js
@@ -0,0 +1,307 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"Hi &#128075;",
+				"I'm Vinay Somawat. I’m currently working as a Software Engineer at Biofourmis India specialized in JavaScript Applications. I did my undergrad in CS from NIT Warangal.",
+				"I'm a developer, geek and curious human besides being an OpenSource enthusiast. I have experience of delivering solutions for interesting problems from startup and industry space.",
+				"I love to read, listen to music, and travel.",
+			],
+		},
+		contact: {
+			text: [
+				"If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email. My email address is: vinaysomawat40@gmail.com",
+			],
+		},
+	},
+	skills: [
+		{
+			title: "Languages",
+			skillName: "JavaScript, C++, Java, C#, Python, SQL, PHP, Swift, Objective-C, R",
+			color: "1",
+			percentage: "78",
+		},
+		{
+			title: "Frameworks/Libraries",
+			skillName: "Angular, Reactjs, NumPy, Bootstrap, Vuejs, NET",
+			color: "6",
+			percentage: "50",
+		},
+		{
+			title: "Backend",
+			skillName: "Nodejs, MongoDB",
+			color: "2",
+			percentage: "60",
+		},
+		{
+			title: "Clouds",
+			skillName: "AWS(EC2, S3), Heroku, Netlify",
+			color: "3",
+			percentage: "40",
+		},
+		{
+			title: "Design",
+			skillName: "HTML, Bootstrap, SCSS, CSS",
+			color: "4",
+			percentage: "90",
+		},
+		{
+			title: "Version Control",
+			skillName: "GitHub, JIRA, Trello, Azure DevOps, Maven",
+			color: "7",
+			percentage: "90",
+		},
+		{
+			title: "Tools",
+			skillName: "Postman, Chrome DevTools",
+			color: "3",
+			percentage: "90",
+		},
+		{
+			title: "QA Testing Tools",
+			skillName: "Selenium, Appium",
+			color: "5",
+			percentage: "40",
+		},
+		{
+			title: "Editor",
+			skillName: "VS Code, Eclipse, Android Studio",
+			color: "6",
+			percentage: "90",
+		},
+	],
+	projects: {
+		web: [
+			{
+				projectName: "PokeDex",
+				image: "images/programmingdiaries.png",
+				summary:
+					"Developed a Pokedex from scratch fetched from API.",
+				techStack: [
+					"JavaScript",
+					"HTML",
+					"CSS",
+					"jQuery",
+					"Bootstrap",
+				],
+			},
+			{
+				projectName: "Movie API",
+				image: "images/findyourbank.png",
+				summary:
+					"Developed a movie API.",
+				techStack: ["Reactjs", "Bootstrap", "JavaScript", "Heroku"],
+			},
+			{
+				projectName: "Web Portfolio",
+				image: "images/portfolio.png",
+				summary:
+					"As another way to showcase my skill, this very portfolio is another project.",
+				preview: "https://github.com/vinaysomawat/Travographer-Portal",
+				techStack: ["HTML", "Bootstrap", "JavaScript"],
+			},
+			{
+				projectName: "To Do App",
+				image: "images/resume-builder.png",
+				summary:
+					"A simple to do app.",
+				preview: "https://vinaysomawat.github.io/Resume-Builder",
+				techStack: ["HTML", "Bootstrap", "JavaScript"],
+			},
+		],
+		software: [
+			{
+				projectName: "Nice Guys",
+				image: "images/pizzaorderchatbot.png",
+				summary:
+					"Forum app for users to protect themselves from predoators.",
+				preview:
+					"https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
+				techStack: ["Backend", "Frontend"],
+			},
+			{
+				projectName: "Discord-Bot",
+				image: "images/whatsappbot.jpg",
+				summary:
+					"Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
+				preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
+				techStack: ["Selenium", "Chrome Webdriver", "Python"],
+			},
+			{
+				projectName: "Bill Generator",
+				image: "images/billgenerator.png",
+				summary:
+					"GUI to transfer data to excel sheets and generate bills on the local shops.",
+				preview: "https://github.com/vinaysomawat/Bill-Generator",
+				techStack: ["Tkinter", "Openxlpy", "Python"],
+			},
+		],
+		android: [
+			{
+				projectName: "NITW-CSE",
+				image: "images/nitwcse.jpg",
+				summary:
+					"The Application display details of Department courses, reference books, research, publication and faculty profile.",
+				preview: "https://github.com/vinaysomawat/NITW-CSE",
+				techStack: ["JAVA", "XML", "Android"],
+			},
+			{
+				projectName: "CareerHigh-App",
+				image: "images/carrerhigh.png",
+				summary:
+					"The Application display the webpages of website careerhigh.in in android devices.",
+				preview: "https://github.com/vinaysomawat/CareerHigh-Android",
+				techStack: ["JAVA", "XML", "Android"],
+			},
+		],
+		freelance: [
+			{
+				projectName: "SnylloAir.com",
+				image: "images/snylloair.png",
+				summary:
+					"Developed a company website to showcase the purpose, services and products provided by the company to audience.",
+				preview: "https://www.snylloair.com/",
+				techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
+			},
+			{
+				projectName: "Delivery+",
+				image: "images/AM-Logo-.png",
+				summary:
+					"Android Application to display website in android devices.",
+				preview:
+					"https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
+				techStack: ["Android", "JAVA", "Play Store"],
+			},
+		],
+	},
+	experience: [
+		{
+			title: "Selenium Internship Training.",
+			duration: "July 2021 - March 2022",
+			subtitle: "QA Testing Intern",
+			details: [
+				"Trained in QA Engineering using Selenium WebDriver, Java, TestNG, and Maven."
+			],
+			tags: [
+				"Java",
+				"Maven",
+				"Selenium",
+				"TestNG",
+				"Postman",
+			],
+			icon: "heartbeat",
+		},
+		{
+			title: "Digital Punk Studios",
+			duration: "TBA",
+			subtitle: "Junior App Developer",
+			details: [
+				"Implemented Aadhaar Enabled Payment services such as Bio-metric eKYC, Cash Withdrawal, Balance Enquiry, Mini-Statements, and Money transfer; completed more than 20 story points in each sprint.",
+				"Integrated QR Code and reduced the effective time by 50 percent to load money into wallet, Clevertap events to track user actions, Freshdesk ticketing service and chat-bot services. Worked on user onboarding, approval, and finance interfaces.",
+				"Co-ordinated closely with the product team, backend team, android team, and QA team to deliver the product builds before deadlines.",
+			],
+			tags: [
+				"JavaScript",
+				"Angular",
+				"React",
+				"Bootstrap",
+				"Nodejs",
+				"Jenkins",
+			],
+			icon: "qrcode",
+		},
+		{
+			title: "Global Tell Link",
+			duration: "May 2019 - June 2019",
+			subtitle: "SDE Intern",
+			details: [
+				"Worked as a full stack developer to support tech team.",
+				"Developed a customer Web Application from scratch for social media management.",
+			],
+			tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
+			icon: "group",
+		},
+	],
+	education: [
+		{
+			title: "Bachelors in Computer Science and Engineering",
+			duration: "",
+			subtitle: "Saint Leo University WorldWide",
+			details: [
+				"Dean Lister in Fall 2020",
+				"Omega Nu Lambda and other Honors.",
+				"Dived into various programming languages such as C, Java, and C++.",
+			],
+			tags: [
+				"Data Structures & Algorithms",
+				"Operating Systems",
+				"Software Engineering",
+				"Java",
+				"SDLC",
+				"Cloud Computing",
+			],
+			icon: "graduation-cap",
+		},
+		{
+			title: "Class 11-12th in Science and Mathematics",
+			duration: "",
+			subtitle: "Board of Secondary Education, Rajasthan",
+			details: [
+				"Qualified JEE Advanced, Main & BITSAT.",
+				"Secured 99.5 percentile in Class 12th Boards Examinations.",
+			],
+			tags: ["Physics", "Chemistry", "Mathematics"],
+			icon: "book",
+		},
+	],
+	footer: [
+		{
+			label: "Dev Profiles",
+			data: [
+				{
+					text: "Stackoverflow",
+					link: "https://stackoverflow.com/users/8461233/vinay-somawat",
+				},
+				{
+					text: "GitHub",
+					link: "https://github.com/vinaysomawat",
+				},
+				{
+					text: "LeetCode",
+					link: "https://leetcode.com/somawatvinay/",
+				},
+			],
+		},
+		{
+			label: "Resources",
+			data: [
+				{
+					text: "Enable Dark/Light Mode",
+					func: "enableDarkMode()",
+				},
+				{
+					text: "Print this page",
+					func: "window.print()",
+				},
+				{
+					text: "Clone this page",
+					link: "https://github.com/vinaysomawat/vinaysomawat.github.io",
+				},
+			],
+		},
+		{
+			label: "Social Profiles",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://www.linkedin.com/in/samiamtn/",
+				},
+				
+				{
+					text: "GitHub",
+					link: "https://www.samia-gits-it.github.com/",
+				},
+			],
+		},
+	],
+};
